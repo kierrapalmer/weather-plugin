31fc3dff64f0d2868a1619c370df445b5d810090
kierrapalmer <kierrapalmer@aol.com>
2019-04-29 22:37:20 -0600

    Fixed long wait times by reorganizing code. Last commit of night

--- my-weather-forecast.php
+++ my-weather-forecast.php
@@ -7,11 +7,17 @@
 	License: GPLv2 or later
 	 * Converted initial program to wordpress theme and plugin
 	 */
-	function get_weather_forecast($zip){
+
+
+	function get_weather_forecast(){
 		$url = 'http://api.apixu.com/v1';
 		$authToken = 'e690024b3b2b4122bcb140627192704';
 
-		$forecast = curl_init($url . '/forecast.json?key=' . $authToken . '&q=' . $zip . '&days=5');
+		if(isset($_POST['location']))
+			$location = $_POST['location'];
+		else
+			$location = 'New York City';
+		$forecast = curl_init($url . '/forecast.json?key=' . $authToken . '&q=' . $location . '&days=5');
 		curl_setopt_array($forecast, array(
 			CURLOPT_POST => TRUE,
 			CURLOPT_RETURNTRANSFER => TRUE,
@@ -33,88 +39,71 @@
 		// Decode and return the response
 		return json_decode($responseForecast, TRUE);
 	}
-	function get_weather( $atts) {
-		$atts = shortcode_atts( array(
-			'zip'  => '0',
-			'day'   => null,
-			'measurement' => ''
-		), $atts, 'weather_forecast' );
 
-		//Error checking
-		if($atts['zip'] == '0'){
-			die("No Zip entered");
-		}else if($atts['measurement'] == ''){
-			die("No measurement entered");
-		}else if($atts['day'] == null){
-			die("No day entered");
-		}
-
-		//Call API and get weather forecast
-		$responseData = get_weather_forecast($atts['zip']);
-
-
-		if($atts['day'] == 5) {
-			if($atts['measurement'] == 'conditionIcon')
-				return $responseData['current']['condition']['icon'];
-			else if($atts['measurement'] == 'conditionText')
-				return $responseData['current']['condition']['text'];
-
-			return $responseData['current'][$atts['measurement']];
-		}
-		else {
-			if($atts['measurement'] == 'conditionIcon')
-				return $responseData['forecast']['forecastday'][$atts['day']]['day']['condition']['icon'];
-			else if($atts['measurement'] == 'conditionText')
-				return $responseData['forecast']['forecastday'][$atts['day']]['day']['condition']['text'];
-
-			return $responseData['forecast']['forecastday'][$atts['day']]['day'][$atts['measurement']];
-		}
+	function format_date($date){
+		$newDate=date_create($date);
+		return date_format($newDate,"m/d");
 	}
 
-	function get_location($atts){
-		$atts = shortcode_atts( array(
-			'zip'  => '0',
-		), $atts, 'forecast_location' );
+	add_shortcode( "get_forecast", "print_layout" );
 
-		//Error checking
-		if($atts['zip'] == '0'){
-			die("No Zip entered");
+
+
+	function print_layout(){
+		$responseData= get_weather_forecast();
+
+		echo $responseData;
+		echo '<div id="currentWeather" class="card">';
+			echo '<div class="card-header">';
+				echo '<form action="#" method="post" id="zipForm">';
+					echo '<input type="text" name="location" id="location" value="';
+					echo $responseData['location']['name'] . ', ' . $responseData['location']['region'] . '">';
+					echo '<input type="submit">';
+				echo '</form>';
+
+				echo '<img src="';
+				echo $responseData['current']['condition']['icon'] . '" alt="condition icon">';
+
+				echo'<h1 class="currentTemp">';
+				echo $responseData["current"]["temp_f"] . '&deg; </h1>';
+
+				echo '<h3 class="conditionText">';
+				echo $responseData['current']['condition']['text'] . '</h3>	';
+
+				echo '<p>H ';
+				echo $responseData['forecast']['forecastday'][0]['day']['maxtemp_f'] . '&deg; L ';
+				echo $responseData['forecast']['forecastday'][0]['day']['mintemp_f'] . '&deg; </p>';
+
+				echo '<p class="humidity"> Humidity: ';
+				echo $responseData['current']['humidity'] . '&percnt; </p>';
+
+				echo '<p class="wind">Wind: ';
+				echo $responseData['current']['wind_mph'] . ' mph ';
+				echo $responseData['current']['wind_dir'] . '</p>';
+			echo '</div>';    //end of card-header div
+
+
+		//loop through next 4 days
+		echo '<table>';
+		for($day=1; $day<5; $day++){
+			echo '<tr class="forecastRow" >' ;
+				echo '<td class="date">';
+				echo format_date($responseData['forecast']['forecastday'][$day]['date']) . '</td>';
+
+				echo '<td> <img src="';
+				echo $responseData['forecast']['forecastday'][$day]['day']['condition']['icon'] . '" class="forecastIcon" alt="forecast icon"> </td>';
+
+				echo '<td><i class="fas fa-tint"></i>';
+				echo $responseData['forecast']['forecastday'][$day]['day']['avghumidity'] . '&percnt; </td>';
+
+				echo'<td class="temp">';
+				echo $responseData['forecast']['forecastday'][$day]['day']['mintemp_f'] . '&deg; /';
+				echo $responseData['forecast']['forecastday'][$day]['day']['maxtemp_f'] . '&deg; </td>';
+			echo '</tr>';
 		}
-
-		//Call API and get weather forecast
-		$responseData = get_weather_forecast($atts['zip']);
-
-		return $responseData['location']['name'] . ', ' . $responseData['location']['region'];
-
+		echo '</table>';
+		echo '</div>';
 	}
 
-	function get_date($atts){
-		$atts = shortcode_atts( array(
-			'zip'  => '0',
-			'day'   => null,
-		), $atts, 'forecast_date' );
 
-		//Error checking
-		if($atts['zip'] == '0'){
-			die("No Zip entered");
-		}else if($atts['day'] == null){
-			die("No day entered");
-		}
-
-		//Call API and get weather forecast
-		$responseData = get_weather_forecast($atts['zip']);
-		$date=date_create($responseData['forecast']['forecastday'][$atts['day']]['date']);
-		return date_format($date,"m/d");
-
-	}
-
-	add_shortcode( "weather_forecast", "get_weather" );
-	add_shortcode( "forecast_location", "get_location" );
-	add_shortcode( "forecast_date", "get_date" );
-
-
-	//		$zip = $_POST['zip'];
-
-
-?>
 
